c46a814ed234b2d2f9b7e7c77eb1d3e564d8f135
renzorainier <114910204+renzorainier@users.noreply.github.com>
2024-07-25 05:42:15 +0000

    Thu, Jul 25, 2024, 5:42 AM +00:00

--- src/app/components/Scan.jsx
+++ src/app/components/Scan.jsx
@@ -11,51 +11,91 @@ import alreadyScannedSound from "./alreadyscanned.wav"; // Import the already sc
 // Import message sounds for check-in and check-out modes
 import complete from "./complete.wav";
 const checkInMessages = [complete];
-
 const checkOutMessages = [complete];
 
 function Scan() {
+  // State Hooks
   const [data, setData] = useState("");
   const [log, setLog] = useState([]);
   const [studentName, setStudentName] = useState("");
   const [currentDecodedCode, setCurrentDecodedCode] = useState("");
-  const [emailData, setEmailData] = useState({
-    shouldSend: false,
-    decodedCode: "",
-    studentName: "",
-  });
+  const [emailData, setEmailData] = useState({ shouldSend: false, decodedCode: "", studentName: "" });
   const [isCheckInMode, setIsCheckInMode] = useState(null);
-  const [backgroundColor, setBackgroundColor] = useState("bg-gray-100"); // State for background color
+  const [backgroundColor, setBackgroundColor] = useState("bg-gray-100");
 
+  // Ref Hooks
   const scannedCodesRef = useRef(new Set());
-  const lastPlayedRef = useRef(0); // Ref to store the last time the already scanned sound was played
-  const delayTimerRef = useRef(null); // Ref to store the delay timer
+  const lastPlayedRef = useRef(0);
+  const delayTimerRef = useRef(null);
 
+  // Utility Functions
   const checkMode = () => {
     const now = new Date();
     const currentHour = now.getHours();
     const currentMinute = now.getMinutes();
-
-    return (
-      (currentHour > 6 || (currentHour === 6 && currentMinute >= 0)) &&
-      currentHour < 10
-    );
+    return (currentHour > 6 || (currentHour === 6 && currentMinute >= 0)) && currentHour < 10;
   };
-  //try lan
 
+  const cleanup = () => {
+    console.log("Running cleanup...");
+    setData("");
+    setLog([]);
+    setStudentName("");
+    setCurrentDecodedCode("");
+    setEmailData({ shouldSend: false, decodedCode: "", studentName: "" });
+    scannedCodesRef.current.clear();
+  };
+
+  const playSound = (sound) => {
+    const audio = new Audio(sound);
+    audio.play();
+  };
+
+  const triggerVisualFeedback = (color, sound) => {
+    setBackgroundColor(color);
+    playSound(sound);
+    setTimeout(() => setBackgroundColor("bg-gray-100"), 1000);
+  };
+
+  const playRandomMessageSound = (messages) => {
+    const randomIndex = Math.floor(Math.random() * messages.length);
+    const randomSound = messages[randomIndex];
+    playSound(randomSound);
+  };
+
+  const addLogEntry = (processedCode, studentName) => {
+    const newLogEntry = {
+      id: processedCode,
+      time: new Date().toLocaleTimeString("en-US", {
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+      }),
+      studentName: studentName,
+    };
+    console.log("New Log Entry:", newLogEntry);
+    setLog((prevLog) => [newLogEntry, ...prevLog.slice(0, 9)]);
+  };
+
+  const handleEmailSent = () => {
+    setEmailData({ shouldSend: false, decodedCode: "", studentName: "" });
+  };
+
+  const handleScanError = (error) => {
+    console.error("QR Scan Error:", error);
+    triggerVisualFeedback("bg-[#FF0000]", errorSound);
+  };
+
+  // Effect Hooks
   useEffect(() => {
     const initialCheckInMode = checkMode();
     setIsCheckInMode(initialCheckInMode);
-    console.log(
-      `Currently in ${initialCheckInMode ? "check-in" : "check-out"} mode`
-    );
+    console.log(`Currently in ${initialCheckInMode ? "check-in" : "check-out"} mode`);
 
     const interval = setInterval(() => {
       const currentMode = checkMode();
       if (currentMode !== isCheckInMode) {
-        console.log(
-          `Switching to ${currentMode ? "check-in" : "check-out"} mode`
-        );
+        console.log(`Switching to ${currentMode ? "check-in" : "check-out"} mode`);
         setIsCheckInMode(currentMode);
       }
     }, 60000);
@@ -68,25 +108,15 @@ function Scan() {
       const now = new Date();
       const currentHour = now.getHours();
       const currentMinute = now.getMinutes();
-
       if (currentHour === 16 && currentMinute === 3) {
         cleanup();
       }
-    }, 60000); // Check every minute
+    }, 60000);
 
-    return () => clearInterval(cleanupInterval); // Cleanup the interval on component unmount
+    return () => clearInterval(cleanupInterval);
   }, []);
 
-  const cleanup = () => {
-    console.log("Running cleanup...");
-    setData("");
-    setLog([]);
-    setStudentName("");
-    setCurrentDecodedCode("");
-    setEmailData({ shouldSend: false, decodedCode: "", studentName: "" });
-    scannedCodesRef.current.clear();
-  };
-
+  // Main Function
   const updateAttendance = async (decodedCode, isCheckIn) => {
     try {
       const userDocRef = doc(db, "users", decodedCode);
@@ -107,11 +137,7 @@ function Scan() {
             attendance[dateStr] = { checkIn: nowStr, checkOut: null };
             await updateDoc(userDocRef, { attendance });
             console.log("Check-in successful");
-            setEmailData({
-              shouldSend: true,
-              decodedCode,
-              studentName: currentStudentName,
-            });
+            setEmailData({ shouldSend: true, decodedCode, studentName: currentStudentName });
             triggerVisualFeedback("bg-[#06D001]", successSound);
             playRandomMessageSound(checkInMessages);
           } else {
@@ -123,11 +149,7 @@ function Scan() {
               attendance[dateStr].checkOut = nowStr;
               await updateDoc(userDocRef, { attendance });
               console.log("Checkout successful");
-              setEmailData({
-                shouldSend: true,
-                decodedCode,
-                studentName: currentStudentName,
-              });
+              setEmailData({ shouldSend: true, decodedCode, studentName: currentStudentName });
               triggerVisualFeedback("bg-[#06D001]", successSound);
               playRandomMessageSound(checkOutMessages);
             } else {
@@ -138,11 +160,7 @@ function Scan() {
             attendance[dateStr] = { checkIn: null, checkOut: nowStr };
             await updateDoc(userDocRef, { attendance });
             console.log("No check-in recorded but check-out successful");
-            setEmailData({
-              shouldSend: true,
-              decodedCode,
-              studentName: currentStudentName,
-            });
+            setEmailData({ shouldSend: true, decodedCode, studentName: currentStudentName });
             triggerVisualFeedback("bg-[#06D001]", successSound);
             playRandomMessageSound(checkOutMessages);
           }
@@ -163,10 +181,7 @@ function Scan() {
   const handleResult = (result) => {
     if (!!result) {
       const code = result.text;
-      const decodedCode = code
-        .split("")
-        .map((char) => mappingTable[char] || "")
-        .join("");
+      const decodedCode = code.split("").map((char) => mappingTable[char] || "").join("");
 
       if (!decodedCode.startsWith("mvba_")) {
         console.log("Invalid code");
@@ -211,49 +226,8 @@ function Scan() {
     }
   };
 
-  const addLogEntry = (processedCode, studentName) => {
-    const newLogEntry = {
-      id: processedCode,
-      time: new Date().toLocaleTimeString("en-US", {
-        hour: "2-digit",
-        minute: "2-digit",
-        second: "2-digit",
-      }),
-      studentName: studentName,
-    };
-    console.log("New Log Entry:", newLogEntry);
-    setLog((prevLog) => [newLogEntry, ...prevLog.slice(0, 9)]);
-  };
-
-  const handleScanError = (error) => {
-    console.error("QR Scan Error:", error);
-    triggerVisualFeedback("bg-[#FF0000]", errorSound);
-  };
-
-  const handleEmailSent = () => {
-    setEmailData({ shouldSend: false, decodedCode: "", studentName: "" });
-  };
-
-  const playSound = (sound) => {
-    const audio = new Audio(sound);
-    audio.play();
-  };
-
-  const triggerVisualFeedback = (color, sound) => {
-    setBackgroundColor(color);
-    playSound(sound);
-    setTimeout(() => setBackgroundColor("bg-gray-100"), 1000);
-  };
-
-  const playRandomMessageSound = (messages) => {
-    const randomIndex = Math.floor(Math.random() * messages.length);
-    const randomSound = messages[randomIndex];
-    playSound(randomSound);
-  };
-
   return (
-    <div
-      className={`${backgroundColor} flex flex-col lg:flex-row items-center overflow-hidden justify-center min-h-screen p-6 `}>
+    <div className={`${backgroundColor} flex flex-col lg:flex-row items-center overflow-hidden justify-center min-h-screen p-6`}>
       <div className="bg-white rounded-lg shadow-lg p-8 w-full lg:w-1/2 h-full mb-6 lg:mb-0 lg:mr-6 transition-transform transform hover:scale-105">
         <QrReader
           onResult={handleResult}
@@ -268,7 +242,8 @@ function Scan() {
             <p
               className={`text-lg font-semibold ${
                 isCheckInMode ? "text-green-600" : "text-red-600"
-              }`}>
+              }`}
+            >
               {isCheckInMode ? "Check-In Mode" : "Check-Out Mode"}
             </p>
           </div>
@@ -285,7 +260,8 @@ function Scan() {
 
         <div
           className="bg-gray-50 rounded-lg shadow-lg mt-6 w-full overflow-y-scroll"
-          style={{ maxHeight: "300px" }}>
+          style={{ maxHeight: "300px" }}
+        >
           <ul className="text-gray-700 divide-y divide-gray-300 w-full">
             {log.map((entry, index) => (
               <li key={`${entry.id}-${index}`} className="py-4 px-6">
@@ -309,7 +285,47 @@ function Scan() {
     </div>
   );
 }
+
 export default Scan;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
